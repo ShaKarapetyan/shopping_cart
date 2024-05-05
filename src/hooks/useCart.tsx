c67157b2935bb7d572da08c56abac6ac419f073c
Sha_Karapetyan <149888730+ShaKarapetyan@users.noreply.github.com>
2024-05-05 21:02:35 +0400

    2nd commit

---
+++ src/hooks/useCart.tsx
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+import CartContext from "../context/CartProvider";
+import { UseCardContextType } from "../context/CartProvider";
+
+const useCart = (): UseCardContextType => {
+    return useContext(CartContext)
+}
+
+export default useCart
